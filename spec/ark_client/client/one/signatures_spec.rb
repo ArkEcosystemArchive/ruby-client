90d111f18a972cf9d2db97203b3bacfead168540
jaml <dev@jaml.pro>
2018-06-21 21:27:25 +0200

    Fix V1 signatures endpoint tests

--- spec/ark_client/client/one/signatures_spec.rb
+++ spec/ark_client/client/one/signatures_spec.rb
@@ -1,12 +1,13 @@
-describe ArkClient::Client::One::Signatures do
+describe ArkClient::API::One::Signatures do
   before do
     @client = ArkClient::Connection.new({ host: "https://dexplorer.ark.io:8443/api/", version: 1 })
+    @resource = @client.signatures
   end
 
   describe ".fee" do
     it "should be ok" do
-      response = @client.fee
-      expect(response.code).to be 200
+      response = @resource.fee
+      expect(response.status).to be 200
     end
   end
 end
