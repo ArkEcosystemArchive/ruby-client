e6fbc8c19c31a505f627aa5f7160ec01e6e026c5
jaml <dev@jaml.pro>
2018-06-21 18:22:58 +0200

    Fix V1 peers endpoint tests

--- spec/ark_client/client/one/peers_spec.rb
+++ spec/ark_client/client/one/peers_spec.rb
@@ -1,26 +1,27 @@
-describe ArkClient::Client::One::Peers do
+describe ArkClient::API::One::Peers do
   before do
     @client = ArkClient::Connection.new({ host: "https://dexplorer.ark.io:8443/api/", version: 1 })
+    @resource = @client.peers
   end
 
-  describe ".list" do
-    it "should be ok" do
-      response = @client.list
-      expect(response.code).to be 200
+  describe '.list' do
+    it 'should be ok' do
+      response = @resource.list
+      expect(response.status).to be 200
     end
   end
 
-  describe ".show" do
-    it "should be ok" do
-      response = @client.show
-      expect(response.code).to be 200
+  describe '.show' do
+    it 'should be ok' do
+      response = @resource.show('1.1.1.1', 4000)
+      expect(response.status).to be 200
     end
   end
 
-  describe ".version" do
-    it "should be ok" do
-      response = @client.version
-      expect(response.code).to be 200
+  describe '.version' do
+    it 'should be ok' do
+      response = @resource.version
+      expect(response.status).to be 200
     end
   end
 end
