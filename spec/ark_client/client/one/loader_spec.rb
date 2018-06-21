3e4af045f27dd7b80bec4566ad0ce9a1b9a2dcee
jaml <dev@jaml.pro>
2018-06-21 21:26:08 +0200

    Fix V1 loader endpoint tests

--- spec/ark_client/client/one/loader_spec.rb
+++ spec/ark_client/client/one/loader_spec.rb
@@ -1,26 +1,27 @@
-describe ArkClient::Client::One::Loader do
+describe ArkClient::API::One::Loader do
   before do
     @client = ArkClient::Connection.new({ host: "https://dexplorer.ark.io:8443/api/", version: 1 })
+    @resource = @client.loader
   end
 
   describe ".status" do
     it "should be ok" do
-      response = @client.status
-      expect(response.code).to be 200
+      response = @resource.status
+      expect(response.status).to be 200
     end
   end
 
   describe ".sync" do
     it "should be ok" do
-      response = @client.sync
-      expect(response.code).to be 200
+      response = @resource.sync
+      expect(response.status).to be 200
     end
   end
 
   describe ".autoconfigure" do
     it "should be ok" do
-      response = @client.autoconfigure
-      expect(response.code).to be 200
+      response = @resource.autoconfigure
+      expect(response.status).to be 200
     end
   end
 end
