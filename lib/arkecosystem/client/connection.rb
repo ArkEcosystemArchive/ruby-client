6c81ef2e88a4194dfcb930ba0bb6c4ab4d69e7ac
highjhacker <mc-somango@live.fr>
2018-12-09 04:27:13 +0100

    Removed all V1 parts both for the client and the tests

--- lib/arkecosystem/client/connection.rb
+++ lib/arkecosystem/client/connection.rb
@@ -1,11 +1,4 @@
 require 'arkecosystem/client/http/client'
-require 'arkecosystem/client/api/one/accounts'
-require 'arkecosystem/client/api/one/blocks'
-require 'arkecosystem/client/api/one/delegates'
-require 'arkecosystem/client/api/one/loader'
-require 'arkecosystem/client/api/one/peers'
-require 'arkecosystem/client/api/one/signatures'
-require 'arkecosystem/client/api/one/transactions'
 require 'arkecosystem/client/api/two/blocks'
 require 'arkecosystem/client/api/two/delegates'
 require 'arkecosystem/client/api/two/node'
@@ -39,25 +32,11 @@ module ArkEcosystem
         end
       end
 
-      # Return the Accounts API resource.
-      #
-      # @return [Object]
-      def accounts
-        case @version
-        when 1
-          @accounts ||= ArkEcosystem::Client::API::One::Accounts.new(@client)
-        else
-          raise NotImplementedError
-        end
-      end
-
       # Return the Blocks API resource.
       #
       # @return [Object]
       def blocks
         case @version
-        when 1
-          @blocks ||= ArkEcosystem::Client::API::One::Blocks.new(@client)
         when 2
           @blocks ||= ArkEcosystem::Client::API::Two::Blocks.new(@client)
         else
@@ -70,22 +49,8 @@ module ArkEcosystem
       # @return [Object]
       def delegates
         case @version
-        when 1
-          @delegates ||= ArkEcosystem::Client::API::One::Delegates.new(@client)
         when 2
           @delegates ||= ArkEcosystem::Client::API::Two::Delegates.new(@client)
-        else
-          raise NotImplementedError
-        end
-      end
-
-      # Return the Loader API resource.
-      #
-      # @return [Object]
-      def loader
-        case @version
-        when 1
-          @loader ||= ArkEcosystem::Client::API::One::Loader.new(@client)
         else
           raise NotImplementedError
         end
@@ -108,22 +73,8 @@ module ArkEcosystem
       # @return [Object]
       def peers
         case @version
-        when 1
-          @peers ||= ArkEcosystem::Client::API::One::Peers.new(@client)
         when 2
           @peers ||= ArkEcosystem::Client::API::Two::Peers.new(@client)
-        else
-          raise NotImplementedError
-        end
-      end
-
-      # Return the Signatures API resource.
-      #
-      # @return [Object]
-      def signatures
-        case @version
-        when 1
-          @signatures ||= ArkEcosystem::Client::API::One::Signatures.new(@client) # rubocop:disable Metrics/LineLength
         else
           raise NotImplementedError
         end
@@ -134,8 +85,6 @@ module ArkEcosystem
       # @return [Object]
       def transactions
         case @version
-        when 1
-          @transactions ||= ArkEcosystem::Client::API::One::Transactions.new(@client) # rubocop:disable Metrics/LineLength
         when 2
           @transactions ||= ArkEcosystem::Client::API::Two::Transactions.new(@client) # rubocop:disable Metrics/LineLength
         else
@@ -143,7 +92,7 @@ module ArkEcosystem
         end
       end
 
-      # Return the Vptes API resource.
+      # Return the Votes API resource.
       #
       # @return [Object]
       def votes
