b9525300f6d1b76b6a55a9d8431b2f08a4321fb6
dated <hello@dated.fun>
2019-10-18 15:36:07 +0200

    feat: add locks endpoints

---
+++ spec/client/api/locks_spec.rb
@@ -0,0 +1,37 @@
+describe ArkEcosystem::Client::API::Locks do
+  before(:each) do
+    get_connection
+  end
+
+  describe '.all' do
+    it 'should be ok' do
+      response = @connection.locks.all
+      expect(response.url).to eq("#{@host}/locks")
+      expect(response.body['success']).to be_truthy
+    end
+  end
+
+  describe '.show' do
+    it 'should be ok' do
+      response = @connection.locks.show('dummy')
+      expect(response.url).to eq("#{@host}/locks/dummy")
+      expect(response.body['success']).to be_truthy
+    end
+  end
+
+  describe '.search' do
+    it 'should be ok' do
+      response = @connection.locks.search(id: 'dummy')
+      expect(response.url).to eq("#{@host}/locks/search")
+      expect(response.body['success']).to be_truthy
+    end
+  end
+
+  describe '.unlocked' do
+    it 'should be ok' do
+      response = @connection.locks.search(id: 'dummy')
+      expect(response.url).to eq("#{@host}/locks/unlocked")
+      expect(response.body['success']).to be_truthy
+    end
+  end
+end
